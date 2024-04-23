8943597b3d5ee19abcbd26b40693aca3908f75cc
Dev Mittal <146480104+DevMittal09@users.noreply.github.com>
2024-04-23 17:06:23 +0530

    added database

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,5 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, jsonify, request,url_for
+import pymysql
 
+# Establish MySQL connection
+timeout = 10
+conn = pymysql.connect(
+charset="utf8mb4",
+connect_timeout=timeout,
+cursorclass=pymysql.cursors.DictCursor,
+db="WEBSITE",
+host="mysql-14da71f7-devmittal9705-d8fc.d.aivencloud.com",
+password="AVNS_M8EGsbDUEeMajooE7YC",
+read_timeout=timeout,
+port=22874,
+user="avnadmin",
+write_timeout=timeout,
+)
+cursor = conn.cursor()
 app = Flask(__name__)
 
 
@@ -10,9 +26,30 @@ def index():
 
 
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/submit', methods=['post'])
+def submit():
+    try:
+    # Get user input from the form
+        name = request.form['name']
+        phone = request.form['phno']
+        table = request.form['table']
+        print("Name:", name)
+        print("Phone Number:", phone)
+        print("Table Number:", table)
+
+        
+        # Prepare SQL statement with user input
+        sql = "INSERT INTO info (Name, PhoneNo, TableNo) VALUES (%s, %s,%s);"
+        cursor.execute(sql, (name, phone,table))
+
+        # Commit changes and close connection
+        conn.commit()
+
+        conn.close()
+        return 'Data submitted successfully!'
+
+    except Exception as e:
+        return f'Error: {str(e)}'
 
 
 
